6bf84921f15760f8923e97fe77898d1da975c208
风雨兼程 <xiuxian>
2024-06-25 18:24:02 +0800

    feat: 2-6 图标选择器-巧用两次 watch 控制弹框的显示与隐藏 修改 vue 版本为 vue@3.2.16

---
+++ src/views/chooseIcon/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- {{ visible }} -->
+    <m-choose-icon title="选择图标" v-model:visible="visible">
+      选择图标
+    </m-choose-icon>
+  </div>
+</template>
+
+<script setup lang="ts">
+// 引入组件，后面注册为全局组件。
+import mChooseIcon from "../../components/chooseIcon/src/index.vue";
+import { ref } from "vue";
+
+let visible = ref<boolean>(false);
+</script>
+
+<style scoped lang="scss"></style>
