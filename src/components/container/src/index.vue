3d2529b2339b6b7c1e4ff982ce562866d6765575
修仙 <xiuxian>
2024-06-25 11:17:58 +0800

    feat: 2-4 伸缩菜单-完成伸缩菜单基本功能

---
+++ src/components/container/src/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-container>
+    <el-aside width="auto">
+      <el-menu
+        :collapse="isCollapse"
+        default-active="2"
+        class="el-menu-vertical-demo"
+      >
+        <el-menu-item index="1">
+          <el-icon-menu />
+          <span>导航一</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <el-icon-menu />
+          <span>导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <el-icon-menu />
+          <span>导航三</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <span @click="toggle">
+          <el-icon-expand v-if="isCollapse"></el-icon-expand>
+          <el-icon-fold v-else></el-icon-fold>
+        </span>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+let isCollapse = ref(true);
+
+const toggle = () => {
+  isCollapse.value = !isCollapse.value;
+};
+</script>
+
+<style scoped lang="scss">
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
