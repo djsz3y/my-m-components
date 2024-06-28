e309ffa86efd936fd00e1d41b527f7242954953c
风雨兼程 <xiuxian>
2024-06-28 17:56:53 +0800

    feat: 3-3 趋势标记-动态绑定 class 的妙用实现颜色反转

--- src/components/trend/src/index.vue
+++ src/components/trend/src/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="trend">
     <div class="text">
-      {{ text }}
+      <!-- 2）插槽显示文字——利用 vue3 的 useSlot 的特性（小技巧） -->
+
+      <!-- 【2】运用 useSlots() 的特性——命名插槽的名字，默认 default，所以这里是 slots.default -->
+      <slot v-if="slots.default"></slot>
+      <div v-else>{{ text }}</div>
     </div>
     <div class="icon">
+      <!-- 1）实现颜色反转 -->
       <el-icon-arrowup
-        :style="{ color: upIconColor }"
+        :style="{ color: !reverseColor ? upIconColor : '#52c41a' }"
         v-if="type === 'up'"
       ></el-icon-arrowup>
       <el-icon-arrowdown
-        :style="{ color: downIconColor }"
+        :style="{ color: !reverseColor ? downIconColor : '#f5222d' }"
         v-else
       ></el-icon-arrowdown>
     </div>
@@ -17,6 +22,7 @@
 </template>
 
 <script setup lang="ts">
+import { useSlots } from "vue";
 let props = defineProps({
   // 标记当前趋势是上升(up)还是下降(down)
   type: {
@@ -30,6 +36,14 @@ let props = defineProps({
     type: String,
     default: "文字",
   },
+
+  // 1）实现颜色反转
+
+  // 颜色反转只在默认的颜色下生效 如果使用了自定义颜色 这个属性就不生效了
+  reverseColor: {
+    type: Boolean,
+    default: false,
+  },
   // 上升趋势图标颜色
   upIconColor: {
     type: String,
@@ -41,6 +55,12 @@ let props = defineProps({
     default: "#52c41a",
   },
 });
+
+// 2）插槽显示文字——利用 vue3 的 useSlot 的特性（小技巧）
+
+// 【1】运用 useSlots() 的特性——定义
+let slots = useSlots();
+// console.log(slots);
 </script>
 
 <style scoped lang="scss">
--- src/views/trend/index.vue
+++ src/views/trend/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div class="flex">
+    1.实现基础的趋势标记：<br /><br />
+    <!-- <div class="flex">
       <div><m-trend text="营业额"></m-trend></div>
       <div>
         <m-trend text="销售额" type="down"></m-trend>
@@ -12,7 +13,21 @@
       <div>
         <m-trend text="销售额" type="down" downIconColor="red"></m-trend>
       </div>
-    </div>
+    </div> -->
+
+    2.实现 slot 的 text：<br /><br />
+    <!-- <m-trend>营业额</m-trend>
+    <br />
+    <m-trend text="销售业绩"></m-trend> -->
+
+    3.实现颜色反转：<br /><br />
+    <m-trend text="销售额" reverseColor></m-trend>
+    <m-trend
+      text="销售业绩"
+      type="down"
+      reverseColor
+      downIconColor="yellow"
+    ></m-trend>
   </div>
 </template>
 
