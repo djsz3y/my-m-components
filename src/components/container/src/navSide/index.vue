c7d8a5488182bc0cd0302d7e32d2477da12e84cf
风雨兼程 <xiuxian>
2024-07-02 13:57:33 +0800

    feat: 4-5 导航菜单-利用封装好的导航菜单组件改造项目整体结构

--- src/components/container/src/navSide/index.vue
+++ src/components/container/src/navSide/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-menu
+  <!-- <el-menu
     :collapse="collapse"
     default-active="2"
     class="el-menu-vertical-demo"
@@ -16,13 +16,83 @@
       <el-icon-menu />
       <span>趋势标记</span>
     </el-menu-item>
-  </el-menu>
+  </el-menu> -->
+
+  <m-menu
+    :collapse="collapse"
+    :data="data"
+    router
+    :defaultActive="$route.path"
+  ></m-menu>
 </template>
 
 <script setup lang="ts">
 let props = defineProps<{
   collapse: boolean;
 }>();
+
+let data = [
+  {
+    icon: "HomeFilled",
+    name: "首页",
+    index: "/",
+  },
+  {
+    icon: "Check",
+    name: "图标选择器",
+    index: "/chooseIcon",
+  },
+  {
+    icon: "Location",
+    name: "省市区选择",
+    index: "/chooseArea",
+  },
+  {
+    icon: "Sort",
+    name: "趋势标记",
+    index: "/trend",
+  },
+  {
+    icon: "Timer",
+    name: "时间选择",
+    index: "/chooseTime",
+  },
+  {
+    icon: "Bell",
+    name: "通知菜单",
+    index: "/notification",
+  },
+  {
+    icon: "Menu",
+    name: "导航菜单",
+    index: "/menu",
+  },
+  {
+    icon: "TurnOff",
+    name: "城市选择",
+    index: "/chooseCity",
+  },
+  {
+    icon: "DArrowRight",
+    name: "进度条",
+    index: "/progress",
+  },
+  {
+    icon: "ScaleToOriginal",
+    name: "日历",
+    index: "/calendar",
+  },
+  {
+    icon: "Setting",
+    name: "表单",
+    index: "/form",
+  },
+  {
+    icon: "ShoppingBag",
+    name: "表格",
+    index: "/table",
+  },
+];
 </script>
 
 <style scoped lang="scss">
