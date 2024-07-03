73ed578840fd937f965820dc48b744fb721547d4
风雨兼程 <xiuxian>
2024-07-03 20:26:16 +0800

    feat: 4-14 城市选择组件-获取城市数据并显示所有城市

--- src/components/chooseCity/src/index.vue
+++ src/components/chooseCity/src/index.vue
@@ -9,7 +9,7 @@
       <div class="result">
         <div>{{ result }}</div>
         <div>
-          <el-icon-arrowdown></el-icon-arrowdown>
+          <el-icon-arrowdown :class="{ rolate: visible }"></el-icon-arrowdown>
         </div>
       </div>
     </template>
@@ -37,12 +37,35 @@
           </el-select>
         </el-col>
       </el-row>
+      <div class="city">
+        <!-- <div v-for="(value, key) in cities">{{ key }}</div> -->
+        <div class="city-item" v-for="(item, index) in Object.keys(cities)">
+          {{ item }}
+        </div>
+      </div>
+      <el-scrollbar max-height="300px;">
+        <template v-for="(value, key) in cities" :key="key">
+          <el-row style="margin-bottom: 10px">
+            <el-col :span="2">{{ key }}:</el-col>
+            <el-col :span="22" class="city-name">
+              <div
+                class="city-name-item"
+                v-for="(item, index) in value"
+                :key="item.id"
+              >
+                <div>{{ item.name }}</div>
+              </div>
+            </el-col>
+          </el-row>
+        </template>
+      </el-scrollbar>
     </div>
   </el-popover>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import city from "../lib/city";
 
 // 有区域供用户所点击，存在初始值和箭头；
 // 箭头有动画效果，点开向上，再点击向下；
@@ -50,6 +73,7 @@ let result = ref<string>("请选择");
 let visible = ref<boolean>(false);
 let radioValue = ref<string>("按城市");
 let selectValue = ref<string>("");
+let cities = ref(city.cities);
 const options = ref([
   {
     value: "Option1",
@@ -81,8 +105,41 @@ const options = ref([
   width: fit-content;
   cursor: pointer;
 }
+.rolate {
+  transform: rotate(180deg);
+}
 svg {
   width: 1em;
   height: 1em;
+  position: relative;
+  top: 2px;
+  margin-left: 4px;
+  transition: all 0.25s linear;
+}
+.container {
+  padding: 6px;
+}
+.city {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  &-item {
+    padding: 3px 6px;
+    margin-right: 8px;
+    margin-bottom: 8px;
+    border: 1px solid #eee;
+  }
+}
+.city-name {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  &-item {
+    margin-right: 6px;
+    margin-bottom: 6px;
+    cursor: pointer;
+  }
 }
 </style>
