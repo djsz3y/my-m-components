29145f1694671ce46e58886aa3ab653bcc3e7d96
风雨兼程 <xiuxian>
2024-07-03 15:44:12 +0800

    feat: 4-10 时间选择组件-完成时间选择组件的全部功能

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,6 +8,7 @@ import notification from "./notification";
 import list from "./list";
 import menu from "./menu";
 import progress from "./progress";
+import chooseTime from "./chooseTime";
 
 const components = [
   chooseArea,
@@ -19,6 +20,7 @@ const components = [
   list,
   menu,
   progress,
+  chooseTime,
 ];
 
 export default {
---
+++ src/views/chooseTime/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <m-choose-time
+      :startOptions="startOptions"
+      @startChange="startChange"
+      @endChange="endChange"
+    ></m-choose-time>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface endValue {
+  startTime: string;
+  endTime: string;
+}
+let startChange = (val: string) => {
+  console.log("startChange", val);
+};
+let endChange = (val: endValue) => {
+  console.log("endChange", val);
+};
+
+// $attrs.startOptions 把剩余属性 封装进组件
+let startOptions = {
+  // size: "mini",
+  // clearable: false,
+};
+</script>
+
+<style scoped></style>
