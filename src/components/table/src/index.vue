ada32c8c11b460503bc82089f4e86f8915056fff
djsz3y <294764484.qq.com>
2024-11-21 12:37:50 +0800

    feat: 6-3 表格组件-使用 ts 定义表格组件需要的类型

--- src/components/table/src/index.vue
+++ src/components/table/src/index.vue
@@ -2,6 +2,21 @@
   <div>table components</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { TableOptions } from './types'
 
+let props = defineProps({
+  // 表格的配置
+  options: {
+    type: Array as PropType<TableOptions[]>,
+    required: true,
+  },
+  // 表格数据
+  data: {
+    type: Array as PropType<any[]>,
+    required: true,
+  },
+})
+</script>
 <style scoped lang="scss"></style>
