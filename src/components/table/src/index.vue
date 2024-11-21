8ef330d6053b8d722eb8619477def5a6570ec058
djsz3y <294764484.qq.com>
2024-11-21 14:31:21 +0800

    feat: 6-5 表格组件-巧用插槽实现操作项和自定义列数据 1. 使用作用域插槽，增加表格操作项配置“编辑、删除”按钮，实现新增功能的方便性 2. 点击编辑拿到分发的表格的 scope 数据 3. 利用 slot 实现自定义列的数据，且动态绑定唯一的插槽名

--- src/components/table/src/index.vue
+++ src/components/table/src/index.vue
@@ -1,18 +1,46 @@
 <template>
   <el-table :data="data">
-    <template v-for="(item, index) in options" :key="index">
+    <template v-for="(item, index) in tableOptions" :key="index">
+      <!-- 默认非插槽方式展示数据 -->
       <el-table-column
+        v-if="!item.slot"
         :label="item.label"
         :prop="item.prop"
         :align="item.align"
         :width="item.width"
       ></el-table-column>
+      <!-- 插槽方式展示数据 -->
+      <el-table-column
+        v-else
+        :label="item.label"
+        :prop="item.prop"
+        :align="item.align"
+        :width="item.width"
+      >
+        <!-- 插槽方式自定义列 -->
+        <template #default="scope">
+          <slot :name="item.slot" :scope="scope"></slot>
+        </template>
+      </el-table-column>
     </template>
+
+    <!-- 操作项是单独配置 -->
+    <el-table-column
+      :label="actionOptions!.label"
+      :align="actionOptions!.align"
+      :width="actionOptions!.width"
+    >
+      <!-- ele 自带 -->
+      <template #default="scope">
+        <!-- 作用域插槽 分发scope出去 -->
+        <slot name="action" :scope="scope"></slot>
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
 <script setup lang="ts">
-import { PropType } from 'vue'
+import { PropType, computed } from 'vue'
 import { TableOptions } from './types'
 
 let props = defineProps({
@@ -27,5 +55,11 @@ let props = defineProps({
     required: true,
   },
 })
+
+// 过滤操作选项之后的配置
+let tableOptions = computed(() => props.options.filter((item) => !item.action))
+
+// 找出操作项的配置
+let actionOptions = computed(() => props.options.find((item) => item.action))
 </script>
 <style scoped lang="scss"></style>
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <m-table :data="tableData" :options="options"></m-table>
+  <m-table :data="tableData" :options="options">
+    <template #date="{ scope }">
+      <el-icon-timer></el-icon-timer>
+      {{ scope.row.date }}
+    </template>
+    <template #name="{ scope }">
+      <el-popover effect="light" trigger="hover" placement="top" width="auto">
+        <template #default>
+          <div>姓名：{{ scope.row.name }}</div>
+          <div>住址：{{ scope.row.address }}</div>
+        </template>
+        <template #reference>
+          <el-tag>{{ scope.row.name }}</el-tag>
+        </template>
+      </el-popover>
+    </template>
+    <template #action="{ scope }">
+      <el-button size="small" type="primary" @click="edit(scope)"
+        >编辑</el-button
+      >
+      <el-button size="small" type="danger">删除</el-button>
+    </template>
+  </m-table>
 </template>
 
 <script setup lang="ts">
@@ -35,19 +57,36 @@ const options: TableOptions[] = [
     label: '日期',
     prop: 'date',
     align: 'center',
+    slot: 'date',
     // width: 180
   },
   {
     label: '姓名',
     prop: 'name',
     align: 'center',
+    slot: 'name',
   },
   {
     label: '地址',
     prop: 'address',
     align: 'center',
   },
+  // 使用 computed 计算属性把操作选项过滤出来。
+  {
+    label: '操作',
+    align: 'center',
+    action: true,
+  },
 ]
+
+let edit = (scope: any) => {
+  console.log(scope)
+}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+svg {
+  width: 1em;
+  height: 1em;
+}
+</style>
