4e7fc0af2e73d714256425a818a60da8e16f1ce2
djsz3y <294764484.qq.com>
2024-11-21 16:30:35 +0800

    feat: 6-6 表格组件-为表格加上 loading 效果

--- src/components/table/src/index.vue
+++ src/components/table/src/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <el-table :data="data">
+  <el-table
+    :data="data"
+    v-loading="isLoading"
+    :element-loading-text="elementLoadingText"
+    :element-loading-background="elementLoadingBackground"
+    :element-loading-spinner="elementLoadingSpinner"
+    :element-loading-svg="elementLoadingSvg"
+    :element-loading-svg-view-box="elementLoadingSvgViewBox"
+  >
     <template v-for="(item, index) in tableOptions" :key="index">
       <!-- 默认非插槽方式展示数据 -->
       <el-table-column
@@ -54,6 +62,26 @@ let props = defineProps({
     type: Array as PropType<any[]>,
     required: true,
   },
+  // 加载文案
+  elementLoadingText: {
+    type: String,
+  },
+  // 加载背景颜色
+  elementLoadingBackground: {
+    type: String,
+  },
+  // 加载图标名
+  elementLoadingSpinner: {
+    type: String,
+  },
+  // 加载svg
+  elementLoadingSvg: {
+    type: String,
+  },
+  // 加载svg的配置
+  elementLoadingSvgViewBox: {
+    type: String,
+  },
 })
 
 // 过滤操作选项之后的配置
@@ -61,5 +89,8 @@ let tableOptions = computed(() => props.options.filter((item) => !item.action))
 
 // 找出操作项的配置
 let actionOptions = computed(() => props.options.find((item) => item.action))
+
+// 表格是否在加载中
+let isLoading = computed(() => !props.data || !props.data.length) // 没传data || 传了data但是是空数组
 </script>
 <style scoped lang="scss"></style>
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <m-table :data="tableData" :options="options">
+  <m-table
+    :data="tableData"
+    :options="options"
+    elementLoadingText="加载中..."
+    elementLoadingBackground="rgba(0, 0, 0, .8)"
+    :elementLoadingSvg="svg"
+    element-loading-svg-view-box="-10, -10, 50, 50"
+  >
     <template #date="{ scope }">
       <el-icon-timer></el-icon-timer>
       {{ scope.row.date }}
@@ -26,30 +33,51 @@
 
 <script setup lang="ts">
 import { TableOptions } from '../../components/table/src/types'
+import { ref } from 'vue'
 
-// 1.表格数据
-const tableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-]
+interface TableData {
+  date: string
+  name: string
+  address: string
+}
+
+// 1.改造表格数据 3s 后再有
+let tableData = ref<TableData[]>([])
+setTimeout(() => {
+  tableData.value = [
+    {
+      date: '2016-05-03',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-02',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-04',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-01',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+  ]
+}, 3000)
+
+const svg = `
+        <path class="path" d="
+          M 30 15
+          L 28 17
+          M 25.61 25.61
+          A 15 15, 0, 0, 1, 15 30
+          A 15 15, 0, 1, 1, 27.99 7.5
+          L 15 15
+        " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
+      `
 
 // 2.表格配置
 const options: TableOptions[] = [
